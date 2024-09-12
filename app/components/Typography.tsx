6c87798a54855b3ba8ae1c5f8f5ff8e65fb6eed6
Andrew Morrison <morrison.andrew422@gmail.com>
2024-09-11 22:38:16 -0400

    add Input componenet

---
+++ app/components/Typography.tsx
@@ -0,0 +1,14 @@
+type TypographyProps = {
+  variant?: "h1" | "h2" | "h3" | "p" | "caption";
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Typography: React.FC<TypographyProps> = ({
+  variant = "p",
+  className = "",
+  children,
+}) => {
+  const Component = variant;
+  return <Component className={className}>{children}</Component>;
+};
